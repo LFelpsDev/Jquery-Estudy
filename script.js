7214b5566426804a0024217a8a8ef2aae38a9908
Luis Felipe Garção <felipe-mara2003@hotmail.com>
2021-08-18 17:24:02 -0300

    Selecionando Elementos, Find() e Children()

---
+++ script.js
@@ -0,0 +1,11 @@
+let Jquery = $.noConflict()
+
+// Jquery("#lista1 > .item2").hide()
+
+let body = Jquery("body")
+
+// body.children().fadeOut()  Recebe todos os Filhos da lista selecionada
+
+// Jquery("#lista1").click(() => {
+//   lista1.find(".item2").fadeOut(2000)
+// })
