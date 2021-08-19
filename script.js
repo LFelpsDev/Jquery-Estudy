dacdd67a5430d148b36c3fd20f16d9602b5ea8f4
Luis Felipe Garção <felipe-mara2003@hotmail.com>
2021-08-19 18:49:02 -0300

    Modificando CSS, Dimensão e Selecionando Elementos Irmões

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-let lista1 = $("#lista1")
+
+// maneira de buscar todos os irmão do elemento selecionado, ou seja todos elementos que estão no mesmo nível
+
+let selected = $("#div3").siblings()
+
+let nextAll = $("#div3").nextAll() // pega todos elementos irmão porem ele filtra os que vem depois deles por exemplo nesse caso vai retornar #div4 e #div5
+
+let nextUntil = $("#div1").nextUntil("#div5") 
+
+let next = $("#div3").next() // so trais o quem vem logo em seguida
+
+let prev = $("#div3").prev() // pega o quem vem antes dele
+
+let prevAll = $("#div3").prevAll() // pega todos elementos quem vem antes do selecionado
+
+let prevUntil = $("#div5").prevUntil("#div1")
 
 
-lista1.append("<li>Teste</li>") // adiciona um elemento no final da lista
-lista1.prepend("<li>Teste CIma</li>") // Adiciona um elemento no topo da lista
-
-lista1.after("<li>Teste</li>") // criara uma Li, porem quando minha lista selecionada acabar
-lista1.before("<li>Teste</li>") // criara uma Li, porem antes da minha lista selecionada
+console.log(prevUntil)
