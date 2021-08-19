ff492b6d99af9db41e3752cf4b809be9c6faa870
Luis Felipe Garção <felipe-mara2003@hotmail.com>
2021-08-19 15:53:29 -0300

    Removendo Elementos, Inserindo Elementos

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-let jq = $;
+let lista1 = $("#lista1")
 
-console.log(jq("#lista1").text())
+
+lista1.append("<li>Teste</li>") // adiciona um elemento no final da lista
+lista1.prepend("<li>Teste CIma</li>") // Adiciona um elemento no topo da lista
+
+lista1.after("<li>Teste</li>") // criara uma Li, porem quando minha lista selecionada acabar
+lista1.before("<li>Teste</li>") // criara uma Li, porem antes da minha lista selecionada
