9b4aa4be3a9dcf78b497d2d929beaa13e80fb4be
Luis Felipe Garção <felipe-mara2003@hotmail.com>
2021-08-20 15:23:32 -0300

    First, last, eq, not e fim do curso

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 
-// maneira de buscar todos os irmão do elemento selecionado, ou seja todos elementos que estão no mesmo nível
+let selected = $("div")
 
-let selected = $("#div3").siblings()
-
-let nextAll = $("#div3").nextAll() // pega todos elementos irmão porem ele filtra os que vem depois deles por exemplo nesse caso vai retornar #div4 e #div5
-
-let nextUntil = $("#div1").nextUntil("#div5") 
-
-let next = $("#div3").next() // so trais o quem vem logo em seguida
-
-let prev = $("#div3").prev() // pega o quem vem antes dele
-
-let prevAll = $("#div3").prevAll() // pega todos elementos quem vem antes do selecionado
-
-let prevUntil = $("#div5").prevUntil("#div1")
+selected.filter(".div1") // Filtrando Elementos
 
 
-console.log(prevUntil)
+let first = $("#container1").children().first() // pega o primeiro
+let last = $("#container2").children().last() // pega o ultimo elemento
+
+
+let not = $("#container1").children().not(".div2") // pegara os elementos não selecionados
+
+let eq = $("#container1").children().eq(4) // pega o index do array retornado
+
+console.log(eq)
+
+
+
